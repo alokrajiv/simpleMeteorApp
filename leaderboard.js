c1b7b3a3354779dada11597cf1cda5e7ab78dd22
alokrajiv <mail@alokrajiv.com>
2015-06-30 20:57:43 +0400

    Getting started

---
+++ leaderboard.js
@@ -0,0 +1,18 @@
+PlayerList = new Mongo.Collection('players')
+console.log("Print test to console!!")
+/*
+PlayerList.insert({name: "Bob",
+				   score: 0})
+PlayerList.insert({name: "Mary",
+				   score: 0})
+PlayerList.insert({name: "Warren",
+				   score: 0})
+PlayerList.insert({name: "David",
+				   score: 0})
+PlayerList.insert({name: "Bill",
+				   score: 0})
+PlayerList.insert({name: "Tim",
+				   score: 0})
+PlayerList.find().count()				   
+PlayerList.find({name: "David"}).fetch()			   
+*/
