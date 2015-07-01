86e688456df92143719f9f5098274d617e5c3c69
alokrajiv <mail@alokrajiv.com>
2015-07-01 08:33:17 +0400

    Events tried

--- leaderboard.js
+++ leaderboard.js
@@ -2,31 +2,30 @@
 PlayerList = new Mongo.Collection('players')
 console.log("Print test to console!!")
 /*
-PlayerList.insert({name: "Bob",
+** Untested sample input function
+var samplePlayers = ["Bob", "Mary", "Warren", "David", "Bill", "Tim"]
+samplePlayers.forEach(function(data){
+	PlayerList.insert({name: data,
 				   score: 0})
-PlayerList.insert({name: "Mary",
-				   score: 0})
-PlayerList.insert({name: "Warren",
-				   score: 0})
-PlayerList.insert({name: "David",
-				   score: 0})
-PlayerList.insert({name: "Bill",
-				   score: 0})
-PlayerList.insert({name: "Tim",
-				   score: 0})
+})
 PlayerList.find().count()				   
 PlayerList.find({name: "David"}).fetch()			   
 */
 if(Meteor.isClient){
 	console.log("Printed only into client console")
 	Template.leaderboard.helpers({
-	'player' : function() {
-		return PlayerList.find()
-	},
-	'otherPlayer' : function() {
-		return "Some other random text"
-	}
-})
+		'player' : function() {
+			return PlayerList.find() // works if fetch attached as well
+		},
+		'otherPlayer' : function() {
+			return "Some other random text"
+		}
+	})
+	Template.leaderboard.events({
+		'click .tester': function(){
+			alert("click on list element with class tester!!")
+		}
+	})
 }
 if(Meteor.isServer){
 	console.log("Printed only into server console")
